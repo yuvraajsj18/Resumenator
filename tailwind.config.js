62b6fa8a81320087b5418a12da8031d344d5398e
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2021-03-25 17:36:31 +0000

    add htmp prototype folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+        brand: "#0C6BA1",
+      },
+    },
   },
   variants: {
     extend: {},
