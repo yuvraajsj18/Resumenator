f765c65d085f842c7adce792e67c93b55b1d7d5a
himanshu16singh <hsnice16@gmail.com>
2021-04-18 16:26:52 +0530

    job card on job seeker home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import { db } from '../firebase'
 import { useAuth } from '../context/AuthContext'
@@ -7,8 +7,10 @@ const Home = () => {
 
     const { currentUser } = useAuth();
     const history = useHistory();
+    const [jobs, setJobs] = useState([]);
 
     useEffect(() => {
+        let unsub = null;
         document.title = "Home | Resumenator";
 
         const isSetupComplete = async () => {        
@@ -27,6 +29,24 @@ const Home = () => {
         }
 
         isSetupComplete();
+
+        const getJobs = async () => {
+            const jobsCollectionRef = db.collection("jobs");
+            const jobList = [];
+
+            unsub = await jobsCollectionRef.onSnapshot( snapshot => {
+
+                snapshot.forEach(doc => {
+                    jobList.push({id: doc.id, ...doc.data()});
+                });
+
+                setJobs(jobList);
+            });
+        }
+
+        getJobs();
+
+        return unsub;
     });
 
     return (<>
@@ -41,95 +61,50 @@ const Home = () => {
     
             <div>
                 <div className="mt-4 p-2 max-w-xs sm:max-w-lg mx-auto rounded-md text-center mb-3 bg-blue-100 text-blue-900">You have not created a resume yet! <a href="" className="text-brand-dark font-medium underline">Create a resume now.</a></div>
-                <div className="mt-4 p-2 max-w-xs sm:max-w-lg mx-auto rounded-md text-center mb-3 bg-blue-100 text-blue-900">Oops! No Jobs Found.</div>
             </div>
-    
-            <ul className="flex flex-wrap justify-center">
-                <li className="mx-3">
-                    <div className="m-6 max-w-xs sm:max-w-md sm:w-full shadow-md hover:shadow-lg text-center border rounded-md px-3 py-4">
-                    <h2 className="text-2xl">Job Title</h2>
-                    <p className="break-words my-1">Job Summary(First 120 Characters)</p>
-                    <div className="my-1">Experience: 30 years</div>
-                    <div className="my-1">
-                        Skills:
-                        <span className="bg-red-200 rounded-full px-2 py-1 text-sm">Python</span>
-                        <span className="bg-green-200 rounded-full px-2 py-1 text-sm">React</span>
-                        <span className="bg-blue-200 rounded-full px-2 py-1 text-sm">JavaScript</span>
-                    </div>
-                    <button className="border-2 mt-3 px-8 mb-2 py-1 rounded-md text-brand hover:bg-brand hover:text-white font-medium">See More Details</button>
-                    </div>
-                </li>
-                <li className="mx-3">
-                    <div className="m-6 max-w-xs sm:max-w-md sm:w-full shadow-md hover:shadow-lg text-center border rounded-md px-3 py-4">
-                    <h2 className="text-2xl">Job Title</h2>
-                    <p className="break-words my-1">Job Summary(First 120 Characters).</p>
-                    <div className="my-1">Experience: 30 years</div>
-                    <div className="my-1">
-                        Skills:
-                        <span className="bg-red-200 rounded-full px-2 py-1 text-sm">Python</span>
-                        <span className="bg-green-200 rounded-full px-2 py-1 text-sm">React</span>
-                        <span className="bg-blue-200 rounded-full px-2 py-1 text-sm">JavaScript</span>
-                    </div>
-                    <button className="border-2 mt-3 px-8 mb-2 py-1 rounded-md text-brand hover:bg-brand hover:text-white font-medium">See More Details</button>
-                    </div>
-                </li>
-                <li className="mx-3">
-                    <div className="m-6 max-w-xs sm:max-w-md sm:w-full shadow-md hover:shadow-lg text-center border rounded-md px-3 py-4">
-                    <h2 className="text-2xl">Job Title</h2>
-                    <p className="break-words my-1">Job Summary(First 120 Characters).</p>
-                    <div className="my-1">Experience: 30 years</div>
-                    <div className="my-1">
-                        Skills:
-                        <span className="bg-red-200 rounded-full px-2 py-1 text-sm">Python</span>
-                        <span className="bg-green-200 rounded-full px-2 py-1 text-sm">React</span>
-                        <span className="bg-blue-200 rounded-full px-2 py-1 text-sm">JavaScript</span>
-                    </div>
-                    <button className="border-2 mt-3 px-8 mb-2 py-1 rounded-md text-brand hover:bg-brand hover:text-white font-medium">See More Details</button>
-                    </div>
-                </li>
-                <li className="mx-3">
-                    <div className="m-6 max-w-xs sm:max-w-md sm:w-full shadow-md hover:shadow-lg text-center border rounded-md px-3 py-4">
-                        <h2 className="text-2xl">Job Title</h2>
-                        <p className="break-words my-1">Job Summary(First 120 Characters).</p>
-                        <div className="my-1">Experience: 30 years</div>
-                        <div className="my-1">
-                            Skills:
-                            <span className="bg-red-200 rounded-full px-2 py-1 text-sm">Python</span>
-                            <span className="bg-green-200 rounded-full px-2 py-1 text-sm">React</span>
-                            <span className="bg-blue-200 rounded-full px-2 py-1 text-sm">JavaScript</span>
-                        </div>
-                        <button className="border-2 mt-3 px-8 mb-2 py-1 rounded-md text-brand hover:bg-brand hover:text-white font-medium">See More Details</button>
-                    </div>
-                </li>
-                <li className="mx-3">
-                    <div className="m-6 max-w-xs sm:max-w-md sm:w-full shadow-md hover:shadow-lg text-center border rounded-md px-3 py-4">
-                        <h2 className="text-2xl">Job Title</h2>
-                        <p className="break-words my-1">Job Summary(First 120 Characters).</p>
-                        <div className="my-1">Experience: 30 years</div>
-                        <div className="my-1">
-                            Skills:
-                            <span className="bg-red-200 rounded-full px-2 py-1 text-sm">Python</span>
-                            <span className="bg-green-200 rounded-full px-2 py-1 text-sm">React</span>
-                            <span className="bg-blue-200 rounded-full px-2 py-1 text-sm">JavaScript</span>
-                        </div>
-                        <button className="border-2 mt-3 px-8 mb-2 py-1 rounded-md text-brand hover:bg-brand hover:text-white font-medium">See More Details</button>
-                    </div>
-                </li>
-                <li className="mx-3">
-                    <div className="m-6 max-w-xs sm:max-w-md sm:w-full shadow-md hover:shadow-lg text-center border rounded-md px-3 py-4">
-                        <h2 className="text-2xl">Job Title</h2>
-                        <p className="break-words my-1">Job Summary(First 120 Characters).</p>
-                        <div className="my-1">Experience: 30 years</div>
-                        <div className="my-1">
-                            Skills:
-                            <span className="bg-red-200 rounded-full px-2 py-1 text-sm">Python</span>
-                            <span className="bg-green-200 rounded-full px-2 py-1 text-sm">React</span>
-                            <span className="bg-blue-200 rounded-full px-2 py-1 text-sm">JavaScript</span>
-                        </div>
-                        <button className="border-2 mt-3 px-8 mb-2 py-1 rounded-md text-brand hover:bg-brand hover:text-white font-medium">See More Details</button>
-                    </div>
-                </li>
-            </ul>
+
+            {
+                jobs.length > 0 
+                ?
+                <ul className="flex flex-wrap justify-center">
+                    {        
+                        jobs.map(job => {
+
+                            const indexOfFirstFullStop = job.description.indexOf('.');                  
+
+                            /*
+                                Instead of first 120 characters for job summary, now we are showing it till the first
+                                full stop. 
+
+                                Reason for not showing 120 characters:
+                                    - 120 characters may end at a character of a word
+                                
+                                Reason for first full stop:
+                                    - assuming, that every description includes atleast one line.
+                            */
+
+                            return (                        
+                                <li className="mx-3" key={job.id}>
+                                    <div className="m-6 max-w-xs sm:max-w-md sm:w-full shadow-md hover:shadow-lg text-center border rounded-md px-3 py-4">
+                                        <h2 className="text-2xl">{job.title}</h2>
+                                        <p className="break-words my-1">{job.description.substring(0, indexOfFirstFullStop+1)}</p>
+                                        <div className="my-1">Experience: {job.experience_required}</div>
+                                        <div className="my-1">
+                                            Skills:{' '}
+                                            <span className="bg-red-200 rounded-full px-2 py-1 text-sm">{job.skills_required[0]}</span>
+                                            <span className="bg-green-200 rounded-full px-2 py-1 text-sm">{job.skills_required[1]}</span>
+                                            <span className="bg-blue-200 rounded-full px-2 py-1 text-sm">{job.skills_required[2]}</span>
+                                        </div>
+                                        <button className="border-2 mt-3 px-8 mb-2 py-1 rounded-md text-brand hover:bg-brand hover:text-white font-medium">See More Details</button>
+                                    </div>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+                :                
+                <div className="mt-4 p-2 max-w-xs sm:max-w-lg mx-auto rounded-md text-center mb-3 bg-blue-100 text-blue-900">Oops! No Jobs Found.</div>
+            }
         </section>
         </>
       )
